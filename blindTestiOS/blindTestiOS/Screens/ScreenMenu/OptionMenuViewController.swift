0140f4288cf1bdb3f672076f7d0c763c6ad07205
roseeDuMatin <44065511+roseeDuMatin@users.noreply.github.com>
2021-02-09 05:54:05 -0800

    Add AccessoryBrowserDelegate

--- blindTestiOS/blindTestiOS/Screens/ScreenMenu/OptionMenuViewController.swift
+++ blindTestiOS/blindTestiOS/Screens/ScreenMenu/OptionMenuViewController.swift
@@ -6,16 +6,35 @@
 //
 
 import UIKit
+import HomeKit
 
 class OptionMenuViewController: UIViewController {
     @IBOutlet weak var accessoriesTV: UITableView!
     @IBOutlet weak var addBtn: UIButton!
     @IBOutlet weak var backBtn: UIButton!
     
-    var accessories: [String] = []
-    enum Identifier: String {
-        case accessories
-    }
+    var array: [String] = []
+    enum Identifier: String { case accessories }
+    
+    
+    
+    var accessories = [HMAccessory]()
+    var home: HMHome!
+    var room: HMRoom!
+    
+    lazy var accessoryBrowser: HMAccessoryBrowser = {
+      let browser = HMAccessoryBrowser()
+      browser.delegate = self
+      return browser
+      }()
+    var randomHomeName: String = {
+      return "Home \(arc4random_uniform(UInt32.max))"
+      }()
+
+    let roomName = "Bedroom 1"
+
+    var homeManager: HMHomeManager!
+    
     
     class func newInstance() -> OptionMenuViewController {
         let optionMenuVC = OptionMenuViewController()
@@ -24,7 +43,9 @@ class OptionMenuViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        homeManager = HMHomeManager()
+        homeManager.delegate = self
         // Do any additional setup after loading the view.
     }
 
@@ -41,14 +62,42 @@ class OptionMenuViewController: UIViewController {
 extension OptionMenuViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.accessories.count
+        return self.array.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let _: String =  self.accessories[indexPath.row]
+        let _: String =  self.array[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: Identifier.accessories.rawValue, for: indexPath) //as! UITableViewCell
         cell.textLabel?.text = "Test"
         return cell
     }
+}
+
+extension OptionMenuViewController: HMHomeManagerDelegate, HMAccessoryBrowserDelegate {
     
+    func homeManagerDidUpdateHomes(_ manager: HMHomeManager) {
+        manager.addHome(withName: randomHomeName){ (home: HMHome!, error: Error!) in
+
+            if error != nil {
+                print("Could not add the home")
+                return
+            }
+            
+            let strongSelf = self
+            strongSelf.home = home
+            print("Successfully added a home")
+            print("Adding a room to the home...")
+            
+            home.addRoom(withName: strongSelf.roomName){ (room: HMRoom!, error: Error!) in
+                if error != nil {
+                    print("Failed to add a room...")
+                    return
+                }
+                strongSelf.room = room
+                print("Successfully added a room.")
+                print("Discovering accessories now...")
+                strongSelf.accessoryBrowser.startSearchingForNewAccessories()
+            }
+        }
+    }
 }
